c43f7ec7f292f382bb5a48ac267fd8a8e08163de
Shivu <shivanappa.mantur@techvibrant.com>
2021-04-16 13:33:51 +0530

    [1.21] Implemented Launcher Icon And Splash Screen.

---
+++ app/src/main/java/com/security/travelguide/views/main/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.security.travelguide.views.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.security.travelguide.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            // remove title
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.activity_splash);
+
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
